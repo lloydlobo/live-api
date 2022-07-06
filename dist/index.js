a2231a10ec3805d1e7dc69670adec52746aed001
lloydlobo <lloyd@lloydlobo.com>
2022-07-07 04:30:40 +0530

    feat: use axios to get data for newspaper array

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,50 @@
 "use strict";
 console.log("Hello TypeScript");
 var PORT = 8000;
-// initialize
 var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
-// routing in express
-// calling express and saving as app
 var app = express();
+var newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/business",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/uk/money",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/money",
+    base: "https://telegraph.co.uk",
+  },
+];
 var articles = [];
-// path
+newspapers.forEach(function (newspaper) {
+  axios.get(newspaper.address).then(function (response) {
+    var html = response.data;
+    var $ = cheerio.load(html);
+    $('a:contains("cost")', html).each(function () {
+      var title = $(this).text();
+      var url = $(this).attr("href");
+      articles.push({
+        title: title,
+        url: "".concat(newspaper.base).concat(url),
+        source: newspaper.name,
+      });
+      console.log(articles);
+    });
+  });
+});
 app.get("/", function (req, res) {
-    res.json("Welcome to Common News API");
+  res.json("Welcome to Common News API");
 });
 app.get("/news", function (req, res) {
-    axios
-        .get("https://www.theguardian.com/uk/money")
-        .then(function (response) {
-        var html = response.data;
-        console.log(html); // go visit http://localhost:8000/news -> html of the url websites comes back to the terminal
-        // use cheerio to pickup the elements
-        var $ = cheerio.load(html); // allows to pickup elements from html received
-        $('a:contains("cost")', html).each(function () {
-            // grab text of each a that contains the words 'cost' from html
-            var title = $(this).text();
-            var url = $(this).attr("href");
-            //  now declare a global variable pointing to array 'articles[]'
-            articles.push({ title: title, url: url });
-        });
-        res.json(articles); // the port will have this json when we visit
-        // Use JSON Viewer chrome extension to display JSON results formatted
-    });
+  res.json(articles);
 });
-// listen through nodemon - dist/index.js
 app.listen(PORT, function () {
-    console.log("server running at http://localhost:".concat(PORT));
-}); // [1] server running at http://localhost:8000
+  console.log("server running at http://localhost:".concat(PORT));
+});
--- src/index.ts
+++ src/index.ts
@@ -7,37 +7,57 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-// routing in express
-// calling express and saving as app
+// routing in express: calling express and saving as app
 const app = express();
-const articles: { title: string; url: string }[] = [];
+
+// Build a cache of several websites
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/business",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/uk/money",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/money",
+    base: "https://telegraph.co.uk",
+  },
+];
+
+const articles: { title: string; url: string; source: string }[] = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response: { data: any }) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("cost")', html).each(function () {
+      const title: string = $(this).text();
+      const url: string = $(this).attr("href");
+
+      articles.push({
+        title: title,
+        url: `${newspaper.base}${url}`,
+        source: newspaper.name,
+      }); // pass in the base for when the url response misses the base url
+      console.log(articles);
+    });
+  });
+});
+
 // path
 app.get("/", (req: any, res: { json: (arg0: string) => void }) => {
   res.json("Welcome to Common News API");
 });
 
 app.get("/news", (req: any, res: any) => {
-  axios
-    .get(`https://www.theguardian.com/uk/money`)
-    .then((response: { data: any }) => {
-      const html = response.data;
-      console.log(html); // go visit http://localhost:8000/news -> html of the url websites comes back to the terminal
-
-      // use cheerio to pickup the elements
-      const $ = cheerio.load(html); // allows to pickup elements from html received
-
-      $('a:contains("cost")', html).each(function () {
-        // grab text of each a that contains the words 'cost' from html
-        const title: string = $(this).text();
-        const url: string = $(this).attr("href");
-        //  now declare a global variable pointing to array 'articles[]'
-
-        articles.push({ title, url });
-      });
-
-      res.json(articles); // the port will have this json when we visit
-      // Use JSON Viewer chrome extension to display JSON results formatted
-    });
+  res.json(articles); // the port will have this json when we visit
+  // Use JSON Viewer chrome extension to display JSON results formatted
 });
 
 // listen through nodemon - dist/index.js
