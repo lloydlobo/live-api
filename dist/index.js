62cde3a59b4633cd6ada48896af4f7f59f857611
lloydlobo <lloyd@lloydlobo.com>
2022-07-07 05:32:31 +0530

    feat: add sources & parse url data
    fix: remove async from (request, response) when getting `:newspapersId`
    from `app.get("/news/:newspapersId", async...)

--- dist/index.js
+++ dist/index.js
@@ -21,29 +21,113 @@ var newspapers = [
     address: "https://www.telegraph.co.uk/money",
     base: "https://telegraph.co.uk",
   },
+  {
+    name: "cityam",
+    address: "https://www.cityam.com/markets-economics/",
+    base: "",
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/international/section/business",
+    base: "",
+  },
+  {
+    name: "latimes",
+    address: "https://www.latimes.com/business",
+    base: "",
+  },
+  {
+    name: "smh",
+    address: "https://www.smh.com.au/business",
+    base: "https://www.smh.com.au",
+  },
+  {
+    name: "un",
+    address: "https://www.un.org/development/desa/financing/news",
+    base: "",
+  },
+  {
+    name: "bbc",
+    address: "https://www.bbc.co.uk/news/business",
+    base: "https://www.bbc.co.uk",
+  },
+  {
+    name: "es",
+    address: "https://www.standard.co.uk/esmoney",
+    base: "https://www.standard.co.uk",
+  },
+  {
+    name: "sun",
+    address: "https://www.thesun.co.uk/money/",
+    base: "",
+  },
+  {
+    name: "dm",
+    address: "https://www.dailymail.co.uk/news/business",
+    base: "",
+  },
+  {
+    name: "nyp",
+    address: "https://nypost.com/business/",
+    base: "",
+  },
 ];
 var articles = [];
 newspapers.forEach(function (newspaper) {
-  axios.get(newspaper.address).then(function (response) {
-    var html = response.data;
-    var $ = cheerio.load(html);
-    $('a:contains("cost")', html).each(function () {
-      var title = $(this).text();
-      var url = $(this).attr("href");
-      articles.push({
-        title: title,
-        url: "".concat(newspaper.base).concat(url),
-        source: newspaper.name,
+  axios
+    .get(newspaper.address)
+    .then(function (response) {
+      var html = response.data;
+      var $ = cheerio.load(html);
+      $('a:contains("cost")', html).each(function () {
+        var title = $(this).text();
+        var url = $(this).attr("href");
+        articles.push({
+          title: title,
+          url: "".concat(newspaper.base).concat(url),
+          source: newspaper.name,
+        });
+        console.log(articles);
       });
-      console.log(articles);
+    })
+    .catch(function (err) {
+      return console.error(err);
     });
-  });
 });
 app.get("/", function (req, res) {
   res.json("Welcome to Common News API");
 });
 app.get("/news", function (req, res) {
   res.json(articles);
+});
+app.get("/news/:newspapersId", function (req, res) {
+  var newspaperId = req.params.newspapersId;
+  var newspaperAddress = newspapers.filter(function (newspaper) {
+    return newspaper.name === newspaperId;
+  })[0].address;
+  var newspaperBase = newspapers.filter(function (newspaper) {
+    return newspaper.name === newspaperId;
+  })[0].base;
+  axios
+    .get(newspaperAddress)
+    .then(function (response) {
+      var html = response.data;
+      var $ = cheerio.load(html);
+      var articlesSpecific = [];
+      $('a:contains("cost")', html).each(function () {
+        var title = $(this).text();
+        var url = $(this).attr("href");
+        articlesSpecific.push({
+          title: title,
+          url: "".concat(newspaperBase).concat(url),
+          source: newspaperId,
+        });
+      });
+      res.json(articlesSpecific);
+    })
+    .catch(function (err) {
+      return console.error(err);
+    });
 });
 app.listen(PORT, function () {
   console.log("server running at http://localhost:".concat(PORT));
--- src/index.ts
+++ src/index.ts
@@ -27,27 +27,81 @@ const newspapers = [
     address: "https://www.telegraph.co.uk/money",
     base: "https://telegraph.co.uk",
   },
+  {
+    name: "cityam",
+    address: "https://www.cityam.com/markets-economics/",
+    base: "",
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/international/section/business",
+    base: "",
+  },
+  {
+    name: "latimes",
+    address: "https://www.latimes.com/business",
+    base: "",
+  },
+  {
+    name: "smh",
+    address: "https://www.smh.com.au/business",
+    base: "https://www.smh.com.au",
+  },
+  {
+    name: "un",
+    address: "https://www.un.org/development/desa/financing/news",
+    base: "",
+  },
+  {
+    name: "bbc",
+    address: "https://www.bbc.co.uk/news/business",
+    base: "https://www.bbc.co.uk",
+  },
+  {
+    name: "es",
+    address: "https://www.standard.co.uk/esmoney",
+    base: "https://www.standard.co.uk",
+  },
+  {
+    name: "sun",
+    address: "https://www.thesun.co.uk/money/",
+    base: "",
+  },
+  {
+    name: "dm",
+    address: "https://www.dailymail.co.uk/news/business",
+    base: "",
+  },
+  {
+    name: "nyp",
+    address: "https://nypost.com/business/",
+    base: "",
+  },
 ];
 
 const articles: { title: string; url: string; source: string }[] = [];
 
 newspapers.forEach((newspaper) => {
-  axios.get(newspaper.address).then((response: { data: any }) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
+  axios
+    .get(newspaper.address)
+    .then((response: { data: any }) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-    $('a:contains("cost")', html).each(function () {
-      const title: string = $(this).text();
-      const url: string = $(this).attr("href");
+      $('a:contains("cost")', html).each(function () {
+        const title: string = $(this).text();
+        const url: string = $(this).attr("href");
 
-      articles.push({
-        title: title,
-        url: `${newspaper.base}${url}`,
-        source: newspaper.name,
-      }); // pass in the base for when the url response misses the base url
-      console.log(articles);
-    });
-  });
+        articles.push({
+          title: title,
+          url: `${newspaper.base}${url}`,
+          source: newspaper.name,
+        }); // pass in the base for when the url response misses the base url
+
+        console.log(articles);
+      });
+    })
+    .catch((err: any) => console.error(err));
 });
 
 // path
@@ -58,6 +112,45 @@ app.get("/", (req: any, res: { json: (arg0: string) => void }) => {
 app.get("/news", (req: any, res: any) => {
   res.json(articles); // the port will have this json when we visit
   // Use JSON Viewer chrome extension to display JSON results formatted
+});
+
+// get response for a particular news/ url
+app.get("/news/:newspapersId", (req: any, res: any) => {
+  // console.log(req.params.newspapersId);
+  const newspaperId = req.params.newspapersId;
+
+  // filter and get the the json that matches the `:newspapersId` url
+  // const newspaperAddress = newspaper[0].address;
+  const newspaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name === newspaperId
+  )[0].address;
+  // const newspaperBase = newspaper[0].base;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name === newspaperId
+  )[0].base;
+
+  // visit news/guardian --> just get info from guardian
+  axios
+    .get(newspaperAddress)
+    .then((response: { data: any }) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const articlesSpecific: { title: any; url: string; source: any }[] = [];
+      $('a:contains("cost")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+
+        articlesSpecific.push({
+          title: title,
+          url: `${newspaperBase}${url}`,
+          source: newspaperId,
+        });
+      });
+
+      res.json(articlesSpecific);
+    })
+    .catch((err: any) => console.error(err));
 });
 
 // listen through nodemon - dist/index.js
